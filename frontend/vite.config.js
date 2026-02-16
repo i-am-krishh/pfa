7359aed129ff7ee56feee0d8b8a823d3eef43ff8
Krushna Wagh <krushna.wagh@siliconmount.com>
2026-02-17 01:18:10 +0530

    feat: Configure Vite for frontend development and adjust Vercel build output directory.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-import path from "path";
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: path.resolve(__dirname, "../dist"),
-    emptyOutDir: true,
-  }
 })
